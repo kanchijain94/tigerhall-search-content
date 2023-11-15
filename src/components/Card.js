3b2be7287b1a36096959a969e04a2d768346cb3b
prjain <prateekkumar.jain@harman.com>
2023-11-15 15:39:30 +0530

    css changes and added expert name on the card details

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Card({card}) {
   return(
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img className="br-100 h3 w3 dib" alt={card.name} src={card.image.uri} />
+    <div className="tc bg-white dib br3 pa3 ma2 grow bw2 shadow-5 flex">
+      <img className="h3 w3 dib" style={{width:"50%",height:"50%"}} alt={card.name} src={card.image.uri} />
       <div>
         <h2>{card.name}</h2>
-        <p>{card.name}</p>
+        <p>{card.experts[0]["firstName"]} {card.experts[0]["lastName"]}</p>
       </div>
     </div>
   );
